e9f24944c8c69eb673e5d947b15e2dd562126eab
Suhara Vithanage <suha.lyoko@hotmail.com>
2021-06-03 18:44:26 +0530

    added the components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
+  //empty string reset the text after enter
+  const [inputText, setInputText] = useState("");
+  //todo list items
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Suhara's TODO</h1>
       </header>
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+      />
+      <TodoList />
     </div>
   );
 }
